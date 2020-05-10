b79e7e781320941df9cc19d3c78efae1367b1def
zhendesheng <2397646126@qq.com>
2020-05-10 15:07:05 +0800

    再次修改FirstClass

--- git-test-project/src/com/itheima/hit/FirstClass.java
+++ git-test-project/src/com/itheima/hit/FirstClass.java
@@ -10,6 +10,8 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         //hello test
+        //hello test //hello test
+
     }
     public void test(){
         int i =0;
